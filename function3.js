11186559e2aa247f563d4cd49db2dab0c5bd5f96
isisgoncalves <isisvow@gmail.com>
2024-04-22 18:38:35 -0300

    end

--- function3.js
+++ function3.js
@@ -1,1 +1,17 @@
 //Criar um nova função para retornar um valor booleano - verdadeiro ou falso - para verificar e informar se um determinado numero atribuido a uma variavel é primo.
+
+function numPrimo(numero){
+  if (numero <= 1){
+      return false
+  }
+
+  for (let i = 2; i <= numero / 2; i++){
+      if (numero % i === 0){
+          return false
+      }
+  }
+
+  return true
+}
+console.log(numPrimo(7))
+console.log(numPrimo(16))
